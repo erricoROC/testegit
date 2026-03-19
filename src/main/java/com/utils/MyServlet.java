53d5abbae2522af2eda3c28a23be6c1dc26f939c
erricoROC <erricorocino@gmail.com>
2026-03-19 16:00:32 +0100

    Aggiunta riga getWriter a MyServlet.java

--- src/main/java/com/utils/MyServlet.java
+++ src/main/java/com/utils/MyServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class MyServlet
@@ -28,6 +29,7 @@ public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 	}
 
 	/**
